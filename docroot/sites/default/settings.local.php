ee65bd855fa6065e22a4b5ae84c7d98dbd8dcc2f
Ivan Berezhnov <ivan.berezhnov@icloud.com>
2018-12-18 22:00:20 +0200

    DCK-0: Add gulp command to docksal

---
+++ docroot/sites/default/settings.local.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * @file
+ * Local development override configuration feature.
+ *
+ * To activate this feature, copy and rename it such that its path plus
+ * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
+ * 'sites/default/settings.php' and uncomment the commented lines that mention
+ * 'settings.local.php'.
+ *
+ * If you are using a site name in the path, such as 'sites/example.com', copy
+ * this file to 'sites/example.com/settings.local.php', and uncomment the lines
+ * at the bottom of 'sites/example.com/settings.php'.
+ */
+
+use Drupal\Component\Assertion\Handle;
+
+/**
+ * Assertions.
+ *
+ * The Drupal project primarily uses runtime assertions to enforce the
+ * expectations of the API by failing when incorrect calls are made by code
+ * under development.
+ *
+ * @see http://php.net/assert
+ * @see https://www.drupal.org/node/2492225
+ *
+ * If you are using PHP 7.0 it is strongly recommended that you set
+ * zend.assertions=1 in the PHP.ini file (It cannot be changed from .htaccess
+ * or runtime) on development machines and to 0 in production.
+ *
+ * @see https://wiki.php.net/rfc/expectations
+ */
+assert_options(ASSERT_ACTIVE, TRUE);
+Handle::register();
+
+
+/**
+ * Show all error messages, with backtrace information.
+ *
+ * In case the error level could not be fetched from the database, as for
+ * example the database connection failed, we rely only on this value.
+ */
+$config['system.logging']['error_level'] = 'verbose';
+
+/**
+ * Disable CSS and JS aggregation.
+ */
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+
+/**
+ * Allow test modules and themes to be installed.
+ *
+ * Drupal ignores test modules and themes by default for performance reasons.
+ * During development it can be useful to install test extensions for debugging
+ * purposes.
+ */
+$settings['extension_discovery_scan_tests'] = TRUE;
+
+/**
+ * Enable access to rebuild.php.
+ *
+ * This setting can be enabled to allow Drupal's php and database cached
+ * storage to be cleared via the rebuild.php page. Access to this page can also
+ * be gained by generating a query string from rebuild_token_calculator.sh and
+ * using these parameters in a request to rebuild.php.
+ */
+$settings['rebuild_access'] = FALSE;
+
+/**
+ * Skip file system permissions hardening.
+ *
+ * The system module will periodically check the permissions of your site's
+ * site directory to ensure that it is not writable by the website user. For
+ * sites that are managed with a version control system, this can cause problems
+ * when files in that directory such as settings.php are updated, because the
+ * user pulling in the changes won't have permissions to modify files in the
+ * directory.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
+
+$databases['default']['default'] = array (
+  'database' => 'default',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = '../config/sync_dev';
+$config['system.logging']['error_level'] = 'verbose';
+
+/*
+ * To Enable local development services - put the file .env with text "dev" to
+ * DRUPAL_ROOT . '/sites/default.
+ */
+$env = 'staging';
+
+if (file_exists("$app_root/$site_path/.env")) {
+  $env = trim(file_get_contents($app_root . '/' . $site_path . '/' . '.env'));
+}
+
+if ($env == 'dev') {
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+  $settings['cache']['bins']['render'] = 'cache.backend.null';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+  $config['system.performance']['css']['preprocess'] = FALSE;
+  $config['system.performance']['js']['preprocess'] = FALSE;
+
+  $config['system.mail']['interface']['default'] = 'devel_mail_log';
+  $config['mailsystem.settings']['defaults']['sender'] = 'devel_mail_log';
+  $config['mailsystem.settings']['modules']['swiftmailer']['none']['sender'] = 'devel_mail_log';
+  $config['mailsystem.settings']['modules']['user']['none']['sender'] = 'devel_mail_log';
+
+  $config['devel.settings']['debug_mail_directory'] = 'public://maildebug';
+}
+
+// Workaround for permission issues with NFS shares in Vagrant.
+$settings['file_chmod_directory'] = 0777;
+$settings['file_chmod_file'] = 0666;
