505d003f23f04a13357cc56fa1444f3530de70ac
Anatoliy Polyakov <anpolimus_dz51@ua.fm>
2019-02-12 23:41:09 +0200

    Prod build script

---
+++ build-prod.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+HOME="/var/www/drupalcampkyiv.org"
+
+echo "Getting last repository update";
+
+cd $HOME
+git pull
+
+echo "Building composer packages"
+cd $HOME
+composer install
+
+echo "Importing config and updating database"
+cd $HOME
+./vendor/bin drush updb -y
+./vendor/bin drush cim -y
+
+
+echo "Building theme"
+
+cd $HOME/docroot/themes/custom/dckyiv
+npm install
+gulp build
