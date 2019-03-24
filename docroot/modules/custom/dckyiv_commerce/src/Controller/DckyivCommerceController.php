d2c924da5ff1d631a36afcfebc50fcdb45634b37
Andriy Iun <andriy.yun@gmail.com>
2019-03-25 00:22:08 +0100

    DCK-51 Added attendee paragraph with functionality to set attende names.

---
+++ docroot/modules/custom/dckyiv_commerce/src/Controller/DckyivCommerceController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\dckyiv_commerce\Controller;
+
+use Drupal\commerce_order\Entity\OrderItemInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\user\UserInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * DckyivCommerceController.
+ */
+class DckyivCommerceController extends ControllerBase {
+
+  /**
+   * Attende edit form callback page.
+   *
+   * @return array
+   *   The attendee form.
+   */
+  public function attendeeFormEdit(UserInterface $user, OrderItemInterface $commerce_order_item, ParagraphInterface $attendee_paragraph) {
+    if ($commerce_order_item != $attendee_paragraph->getParentEntity()->id()
+      || $commerce_order_item->getOrder()->getCustomerId() != $user->id()) {
+      throw new AccessDeniedHttpException();
+    }
+    $form = \Drupal::service('entity.form_builder')->getForm($attendee_paragraph, 'edit');
+    return $form;
+  }
+
+  /**
+   * Attende add form callback page.
+   *
+   * @param UserInterface $user
+   *   The user object.
+   * @param OrderItemInterface $commerce_order_item
+   *   The commerce order item object.
+   *
+   * @throws
+   *
+   * @return array
+   *   The attendee form.
+   */
+  public function attendeeFormAdd(UserInterface $user, OrderItemInterface $commerce_order_item) {
+    /** @var ParagraphInterface $attendee_paragraph */
+    $attendee_paragraph = Paragraph::create(['type' => 'attendee']);
+    $attendee_paragraph->isNew();
+    $form = \Drupal::service('entity.form_builder')->getForm($attendee_paragraph, 'default');
+    return $form;
+  }
+
+}
---
+++ docroot/modules/custom/dckyiv_commerce/src/Plugin/Field/FieldFormatter/AttendeeFormatter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\dckyiv_commerce\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Url;
+use Drupal\entity_reference_revisions\Plugin\Field\FieldFormatter\EntityReferenceRevisionsEntityFormatter;
+
+/**
+ * Plugin implementation of the 'paragraph_summary' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "paragraph_attendee",
+ *   label = @Translation("Attendee rendered entity"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   }
+ * )
+ */
+class AttendeeFormatter extends EntityReferenceRevisionsEntityFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $view_mode = $this->getSetting('view_mode');
+    $elements = array();
+
+    $commerce_order_item = $items->getParent()->getValue();
+    $quantity = (int) $commerce_order_item->getQuantity();
+
+    $entities = $this->getEntitiesToView($items, $langcode);
+    for ($delta = 0; $delta < $quantity; $delta++) {
+      if (isset($entities[$delta])) {
+        $entity = $entities[$delta];
+        // Protect ourselves from recursive rendering.
+        static $depth = 0;
+        $depth++;
+        if ($depth > 20) {
+          $this->loggerFactory->get('entity')->error('Recursive rendering detected when rendering entity @entity_type @entity_id. Aborting rendering.', array('@entity_type' => $entity->getEntityTypeId(), '@entity_id' => $entity->id()));
+          return $elements;
+        }
+        $view_builder = \Drupal::entityTypeManager()->getViewBuilder($entity->getEntityTypeId());
+        $elements[$delta] = $view_builder->view($entity, $view_mode, $entity->language()->getId());
+
+        // Add a resource attribute to set the mapping property's value to the
+        // entity's url. Since we don't know what the markup of the entity will
+        // be, we shouldn't rely on it for structured data such as RDFa.
+        if (!empty($items[$delta]->_attributes) && !$entity->isNew() && $entity->hasLinkTemplate('canonical')) {
+          $items[$delta]->_attributes += array('resource' => $entity->toUrl()->toString());
+        }
+        $depth = 0;
+      }
+      else {
+        $elements[$delta] = [
+          '#type' => 'container',
+          '#attributes' => ['class' => ['attendee--wrapper']],
+          'attendee' => [
+            '#type' => 'link',
+            '#title' => 'Add attendee name',
+            '#url' => Url::fromRoute('dckyiv_commerce.attendee_add', [
+              'user' => 1,
+              'commerce_order_item' => $commerce_order_item->id(),
+            ], [
+              'query' => [
+                'destination' => \Drupal::service('path.current')->getPath(),
+              ]
+            ]),
+            '#attributes' => ['class' => ['attendee--add-info']],
+          ]
+        ];
+      }
+    }
+
+    return $elements;
+  }
+
+}
