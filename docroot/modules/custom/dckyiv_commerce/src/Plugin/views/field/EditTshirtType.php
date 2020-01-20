6c92adcef7d310f5261883b750f493d3271cc98a
Anatoliy Polyakov <anpolimus@gmail.com>
2020-01-20 13:24:36 +0200

    Sync of sex type at ticket

---
+++ docroot/modules/custom/dckyiv_commerce/src/Plugin/views/field/EditTshirtType.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Drupal\dckyiv_commerce\Plugin\views\field;
+
+use Drupal\commerce_cart\CartManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\Plugin\views\field\UncacheableFieldHandlerTrait;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a form element for editing the order item tshirt type.
+ *
+ * @ViewsField("commerce_order_item_edit_tshirt_type")
+ */
+class EditTshirtType extends FieldPluginBase {
+
+  use UncacheableFieldHandlerTrait;
+
+  /**
+   * The cart manager.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected $cartManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a new EditQuantity object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\commerce_cart\CartManagerInterface $cart_manager
+   *   The cart manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CartManagerInterface $cart_manager, EntityTypeManagerInterface $entity_type_manager, MessengerInterface $messenger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->cartManager = $cart_manager;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('commerce_cart.cart_manager'),
+      $container->get('entity_type.manager'),
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clickSortable() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue(ResultRow $row, $field = NULL) {
+    return '<!--form-item-' . $this->options['id'] . '--' . $row->index . '-->';
+  }
+
+  /**
+   * Form constructor for the views form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function viewsForm(array &$form, FormStateInterface $form_state) {
+    // Make sure we do not accidentally cache this form.
+    $form['#cache']['max-age'] = 0;
+    // The view is empty, abort.
+    if (empty($this->view->result)) {
+      unset($form['actions']);
+      return;
+    }
+
+    $form['#attached'] = [
+      'library' => ['commerce_cart/cart_form'],
+    ];
+    $form[$this->options['id']]['#tree'] = TRUE;
+    foreach ($this->view->result as $row_index => $row) {
+      /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
+      $order_item = $this->getEntity($row);
+      $field = $order_item->get('field_t_shirt_type');
+      $defitition = $field->getFieldDefinition();
+      $settings = $defitition->getSettings();
+      $form[$this->options['id']][$row_index] = [
+        '#type' => 'select',
+        '#title' => $defitition->label(),
+        '#title_display' => 'invisible',
+        '#options' => $settings['allowed_values'],
+        '#default_value' => !empty($field[0]->value) ? $field[0]->value : '_none_',
+        '#required' => TRUE,
+      ];
+    }
+    $form['actions']['submit']['#update_cart'] = TRUE;
+    $form['actions']['submit']['#show_update_message'] = TRUE;
+  }
+
+  /**
+   * Submit handler for the views form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @throws
+   */
+  public function viewsFormSubmit(array &$form, FormStateInterface $form_state) {
+    $triggering_element = $form_state->getTriggeringElement();
+    if (empty($triggering_element['#update_cart'])) {
+      // Don't run when the "Remove" or "Empty cart" buttons are pressed.
+      return;
+    }
+
+    $order_storage = $this->entityTypeManager->getStorage('commerce_order');
+    /** @var \Drupal\commerce_order\Entity\OrderInterface $cart */
+    $cart = $order_storage->load($this->view->argument['order_id']->getValue());
+    $tshirt_sizes = $form_state->getValue($this->options['id'], []);
+    $save_cart = FALSE;
+    foreach ($tshirt_sizes as $row_index => $size) {
+      /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
+      $order_item = $this->getEntity($this->view->result[$row_index]);
+      $field = $order_item->get('field_t_shirt_type');
+      if (!empty($field[0]->value) && $field[0]->value == $size) {
+        // The size hasn't changed.
+        continue;
+      }
+
+      $field->setValue($size);
+      $this->cartManager->updateOrderItem($cart, $order_item, FALSE);
+      $save_cart = TRUE;
+    }
+
+    if ($save_cart) {
+      $cart->save();
+      if (!empty($triggering_element['#show_update_message'])) {
+        $this->messenger->addMessage($this->t('Your shopping cart has been updated.'));
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing.
+  }
+
+}
